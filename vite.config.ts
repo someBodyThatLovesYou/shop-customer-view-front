449853c0fc5420b61d75d11398f33cee2f3cbf5b
someBodyThatLovesYou <spotifyPC1287@gmail.com>
2024-04-11 21:42:36 +0330

    vite.config.ts file modified

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/shop-customer-view-front.github.io/" // Adjust this to match your GitHub Pages subdirectory
-      : "/",
-  build: {
-    assetsDir: "assets",
+  define: {
+    "process.env": process.env,
   },
+  server: {
+    host: true,
+  },
+  base: "./",
 });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base:
+//     process.env.NODE_ENV === "production"
+//       ? "/shop-customer-view-front.github.io/" // Adjust this to match your GitHub Pages subdirectory
+//       : "/",
+//   build: {
+//     assetsDir: "assets",
+//   },
+// });
