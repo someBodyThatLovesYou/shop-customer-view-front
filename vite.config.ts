8b04e1dd8c6c6afb3a55dd53ce2e7a82791e842a
someBodyThatLovesYou <spotifyPC1287@gmail.com>
2024-04-11 22:51:28 +0330

    added .yml file and .github folder and modification in vit.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react";
 // // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env,
-  },
-  server: {
-    host: true,
-  },
-  base: "./",
+  // define: {
+  //   "process.env": process.env,
+  // },
+  // server: {
+  //   host: true,
+  // },
+  // base: "./",
+  base: "/ vit-4-0-1-react/",
 });
 
 // export default defineConfig({
