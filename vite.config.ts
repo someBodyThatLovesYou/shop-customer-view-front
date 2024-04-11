11b0cb6f65a38fe29d840e4daa700893e329e42f
mhmd ali <115190542+Seraaga022@users.noreply.github.com>
2024-04-11 14:37:13 +0330

    configured vite.config.ts to use assets instead of default processing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 export default defineConfig({
-  plugins: [react()],
-})
+ plugins: [react()],
+ base: process.env.NODE_ENV === 'production'
+    ? '/project-name/' // Adjust this to match your GitHub Pages subdirectory
+    : '/',
+ build: {
+    assetsDir: 'assets',
+ },
+});
